8feb7bdf6b67cea18cebb5fe76ff19606a03f7e5
Investe USA <75433200+investeusa@users.noreply.github.com>
2023-11-09 16:19:22 -0300

    Update GoogleTagManager.js

--- src/components/GoogleTagManager.js
+++ src/components/GoogleTagManager.js
@@ -8,7 +8,7 @@ export default () =>
       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','G-TLDBN68Z');` }}
+      })(window,document,'script','dataLayer','GTM-TLDBN68Z');` }}
       />
       <noscript dangerouslySetInnerHTML={{
         __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TLDBN68Z"

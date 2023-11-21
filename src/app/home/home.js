486a40c0c8416697d4eec7c190ff9de7f6056549
Investe USA <75433200+investeusa@users.noreply.github.com>
2023-11-21 09:49:30 -0300

    headline4

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -15,7 +15,7 @@ import bannerMeio3 from "@/images/meio3.png";
 import bannerMeio4 from "@/images/meio4.png";
 import garantia from "@/images/garantia-2.png";
 import logoWhite from "@/images/logo.png";
-import headline from "@/images/headline0.png";
+import headline from "@/images/headline4.png";
 import GoogleTagManager from "../../components/GoogleTagManager.js";
 
 export default function Home() {

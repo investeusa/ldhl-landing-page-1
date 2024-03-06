21dee72a700182f5a89d655c4df6fa90f00b2ce0
Investe USA <75433200+investeusa@users.noreply.github.com>
2024-03-06 17:24:08 -0300

    Update home.js

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -121,16 +121,16 @@ export default function Home() {
             )}
             <div className='justify-center items-center content-center relative z-0'>
               <div className="divide-y divide-gray-300/50 m-5 flex z-0">
-                <div id="vid_6577acc932ca9a000972ba62" >
-                  <div id="thumb_6577acc932ca9a000972ba62" role="status" className='flex justify-center items-center p-5 z-0'>
+                <div id="vid_65e8ceeefe98df0008816e61" >
+                  <div id="thumb_65e8ceeefe98df0008816e61" role="status" className='flex justify-center items-center p-5 z-0'>
                     <svg aria-hidden="true" className="inline w-20 h-20 mr-2 text-gray-200 animate-spin dark:text-gray-100 fill-rose-700 z-0" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
                       <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
                     </svg>
                   </div>
-                  <div id="backdrop_6577acc932ca9a000972ba62"></div>
+                  <div id="backdrop_65e8ceeefe98df0008816e61"></div>
                 </div>
-                <Script type="text/javascript" id="scr_6577acc932ca9a000972ba62">var s=document.createElement(`script`);s.src=`https://scripts.converteai.net/dc18b5f5-d9cd-4d50-b62e-13e5bdae39b4/players/6577acc932ca9a000972ba62/player.js`,s.async=!0,document.head.appendChild(s);</Script>
+                <Script type="text/javascript" id="scr_65e8ceeefe98df0008816e61">var s=document.createElement(`script`);s.src=`https://scripts.converteai.net/dc18b5f5-d9cd-4d50-b62e-13e5bdae39b4/players/65e8ceeefe98df0008816e61/player.js`,s.async=!0,document.head.appendChild(s);</Script>
               </div>
             </div>
             <div className="flex justify-center py-7">
